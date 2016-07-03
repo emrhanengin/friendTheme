9604fecdf077c5c91379c649da59e18fc4af254c
emirhanE <demene.besaba@gmail.com>
2016-07-03 14:04:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php ile arkadaşlık sistemi - Emirhan engin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div style="margin: 20px">
+    <a href=''>Çıkış yap</a> | hoşgeldin, <a href=''>Kullanıcı</a> | <a href=''>İstekler</a> : 10
+</div>
+<div class="uyeler">
+    <div class="title">
+        Sitedeki üyeler
+    </div>
+    <div class="icerik">
+        <ul>
+            <li><a href=''>Arkadaş 1</a></li>
+            <li><a href=''>Arkadaş 2</a></li>
+            <li><a href=''>Arkadaş 3</a></li>
+            <li><a href=''>Arkadaş 4</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
